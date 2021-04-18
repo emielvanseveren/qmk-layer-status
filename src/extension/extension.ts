a16731006998702535633c80901019cc8c42be11
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2021-04-19 01:42:22 +0200

    Linking submodules

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,6 +1,8 @@
 import * as Meta from '@imports/Meta-7';
 import { SignalEmitter } from '..';
 
+import ExtensionUtils from '../gselib/extensionUtils';
+
 export declare interface Extension extends SignalEmitter { }
 
 export class Extension {
